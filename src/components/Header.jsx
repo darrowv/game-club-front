2e9d6f59b31c7af069d3bc787336f30adc4c5880
Baysarov <adambajsarov@gmail.com>
2022-04-19 13:57:15 +0300

    adam

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,26 @@ const Header = () => {
   return (
     <header>
       <div className="container">
-        <ul className="list">
-          <Link className="link" to={"/"}>Главня</Link>
-          <Link className="link" to={"/"}>Тарифы</Link>
-          <Link className="link" to={"/"}>Акции</Link>
-          <Link className="link" to={"/"}>Новости</Link>
-          <Link className="link" to={"/"}>Контакты</Link>
-        </ul>
+        <div style={{display: 'flex', alignItems: 'center'}}>
+          <ul className="list">
+            <Link className="link" to={"/"}>
+              Главная
+            </Link>
+            <Link className="link" to={"/"}>
+              Тарифы
+            </Link>
+            <Link className="link" to={"/"}>
+              Акции
+            </Link>
+            <Link className="link" to={"/"}>
+              Новости
+            </Link>
+            <Link className="link" to={"/"}>
+              Контакты
+            </Link>
+          </ul>
+          <button className="signIn">Войти</button>
+        </div>
       </div>
     </header>
   );
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import titleImg from "./img/pubg.png";
+import map from './img/map.png'
 
 const Main = () => {
   return (
@@ -10,7 +11,7 @@ const Main = () => {
         <div className="container">
           <div className="flexTitle">
             <div className="mainImgBlock">
-              <img src={titleImg} alt="none" />
+              <img className="titleImg" src={titleImg} alt="none" />
             </div>
             <div className="titleText">
               <h1>
@@ -18,7 +19,7 @@ const Main = () => {
                 всем мире
               </h1>
               <div className="map">
-                
+                <img style={{width: '100%'}} src={map} alt="" />
               </div>
               <div className="buttons" style={{ display: "flex" }}>
                 <button className="titleBtn">Перейти к брони</button>
