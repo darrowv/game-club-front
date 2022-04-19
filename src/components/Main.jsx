132511a07d6d2c4b40c8fc06027da9548b204529
Baysarov <adambajsarov@gmail.com>
2022-04-19 13:29:27 +0300

    header

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
-import Header from './Header';
+import React from "react";
+import Header from "./Header";
+import titleImg from "./img/pubg.png";
 
 const Main = () => {
   return (
     <>
-    <Header />
-    <div className='container'>
-      
-    </div>
+      <Header />
+      <main>
+        <div className="container">
+          <div className="flexTitle">
+            <div className="mainImgBlock">
+              <img src={titleImg} alt="none" />
+            </div>
+            <div className="titleText">
+              <h1>
+                Лучша работа во <br />
+                всем мире
+              </h1>
+              <div className="map">
+                
+              </div>
+              <div className="buttons" style={{ display: "flex" }}>
+                <button className="titleBtn">Перейти к брони</button>
+                <button className="titleBtn">Позвонить</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   );
 };
