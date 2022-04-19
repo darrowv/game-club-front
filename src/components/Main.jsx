aa238f3f9eccfc8a309fb7ab266b28163c66315a
Mac <nahila_95@mail.ru>
2022-04-19 14:38:00 +0300

    done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import Header from './Header';
+import React from "react";
+import Carts from "./carts/Carts";
+import Header from "./Header";
 
 const Main = () => {
   return (
     <>
-    <Header />
-    <div className='container'>
-      
-    </div>
+      <Header />
+      <div className="container"></div>
+      <Carts />
     </>
   );
 };
---
+++ src/components/carts/Carts.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import styles from "../style.module.css";
+import Carts2 from "./Carts2";
+import Carts3 from "./Carts3";
+
+const Carts = () => {
+  return (
+    <div style={{ marginTop: "50px" }}>
+      <div style={{ display: "flex" }} className={styles.mainDivCars}>
+        <div className={styles.divCarts}>
+          <div>
+            <img
+              src="https://mybrand.kz/wp-content/uploads/2019/11/franchise-united-gamers-1.jpg"
+              className={styles.imgCarts}
+              alt="pc"
+            />
+          </div>
+          <div className={styles.divTextCarts}>
+            <span className={styles.oneSpan}>Так ты еще не играл!</span>
+            <h1 className={styles.h1Cart}>ОСНОВНОЙ ЗАЛ</h1>
+            <div className={styles.textAbout}>
+              <p className={styles.about}>
+                В этом зале конфигурации пк не самая мощная, но также можно на
+                чиле поиграть во все что хочешь <br />
+                И так наша конфигурация в основном зале: <br />
+                AOC 24″ Gaming C24G1 144Hz <br />
+                i5 – 12400 3.4 mhz <br />
+                GeForce 3070TI RTX GigaByte 6Gb <br />
+                Мышь: Razer Abyssus Essential <br />
+                Клавиатура: Razer Cynosa Pro Bundle <br />
+                Наушники: Hyperx cloud X <br />
+              </p>
+              <p className={styles.quantity}>Количество мест</p>
+              <p>от 1 до 20</p>
+            </div>
+            <button className={styles.btnCarts}>Записаться на игру</button>
+          </div>
+        </div>
+        <Carts2 />
+        <Carts3 />
+      </div>
+    </div>
+  );
+};
+
+export default Carts;
