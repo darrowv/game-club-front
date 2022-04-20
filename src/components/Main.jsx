4f92e4b5651ab8fd8f0cc9b7bf803c520981ecd9
Baysarov <adambajsarov@gmail.com>
2022-04-20 11:01:24 +0300

    adam

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,6 +13,7 @@ import img5 from "./img/finding5.svg";
 import img6 from "./img/finding6.svg";
 import "./config.css";
 import "./pages/Arsenal/config.css";
+import Footer from './Footer.jsx'
 
 const Main = () => {
   return (
@@ -102,40 +103,7 @@ const Main = () => {
           answer="Ворзарстного ограничения нет"
         />
       </section>
-      <section className="itemsSection"></section>
-      <section className="finding">
-        
-          <h1 className="arsenal">НАШ АРСЕНАЛ</h1>
-          <div className="config_sec">
-          <ConfigPage
-            img={img1}
-            title="МОЩНЫE ПК"
-            text="С ПРОЦЕССОРАМИ INTEL CORE I7 10700K, I5 10600K, AMD RYZEN 5 3600X.
-16 GB ОПЕРАТИВНОЙ ПАМЯТИ НА ЧАСТОТЕ 4000 MHZ И 2 TB NVME SSD НАКОПИТЕЛИ.
-ВИДЕОКАРТЫ GEFORCE RTX 2060, GTX 1060 (ОЖИДАЕМ ПОСТУПЛЕНИЕ RTX 3070)."
-          />
-          <ConfigPage
-            img={img6}
-            title="МОНИТОРЫ"
-            text="27' AOC C27G2ZU 240 HZ
-          27' MSI OPTIX MAG271CQR 2К, 144 HZ
-          32' MSI OPTIX MAG325CQR 2К, 165 HZ"
-          />
-          <ConfigPage
-            img={img5}
-            title="ПЕРЕФЕРИЯ"
-            text="КЛАВИАТУРЫ HYPERX ALLOY FPS PRO
-          НАУШНИКИ HYPERX CLOUD ALPHA
-          МЫШКИ STEELSERIES RIVAL 710"
-          />
-
-          <ConfigPage
-            img={img2}
-            title="ГЕЙМЕРСКИЕ КРЕСЛА"
-            text="COUGAR RAMPART"
-          />
-        </div>
-      </section>
+      <Footer />
     </>
   );
 };
